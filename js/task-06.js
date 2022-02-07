ee342f1da5302f54ced27de6cfdfbac053fd1ab0
Oleg <mozgarik1613@gmail.com>
2022-02-07 12:02:41 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,11 @@
 const validationInput = document.querySelector('#validation-input');
 const dataLength = validationInput.getAttribute('data-length')
+const dataLengthNum = parseInt(dataLength)
 
 validationInput.addEventListener('blur', onInputChange);
 
 function onInputChange() {
-if (validationInput.value.length === dataLength) {
+if (validationInput.value.length === dataLengthNum) {
 validationInput.classList.remove('invalid')
 validationInput.classList.add('valid');
 } else {
